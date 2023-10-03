87ebf3d511c68e4f36c4f4de1fd3e725d34c1a95
joergenn <53342164+joergenn@users.noreply.github.com>
2023-10-03 15:11:56 +0300

    Revert "Dev"

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -9,38 +9,35 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class TodosService {
   constructor(
     @InjectRepository(Todo)
-    private todos: Repository<Todo>,
+    private todosRepository: Repository<Todo>,
   ){}
   create(createTodoDto: CreateTodoDto) {
-    const newTodo = this.todos.create(createTodoDto);
+    const newTodo = this.todosRepository.create(createTodoDto);
     console.log(newTodo);
-    return this.todos.save(newTodo);
+    return this.todosRepository.save(newTodo);
   }
 
   findAll() {
-    console.log(process.env)
-    return this.todos.find();
+    return this.todosRepository.find();
   }
 
   async findOne(id: number) {
-    const todo = await this.todos.find({
+    return this.todosRepository.find({
       where: {
         id: id
       }
-    });
-    if(Object.keys(todo).length === 0) {
-      throw new HttpException("Todo with such id not found", HttpStatus.NOT_FOUND)
-    }; 
-    return todo;
+    })
   }
 
   async update(id: number, updateTodoDto: UpdateTodoDto) {
-    const todo = await this.findOne(id); 
-    return this.todos.save({...todo[0], ...updateTodoDto});
+    const todo = await this.findOne(id);
+    const user = {...todo, ...updateTodoDto};
+    console.log(user)
+    return this.todosRepository.save({...todo, ...updateTodoDto});
   }
 
   async remove(id: number) {
     const todo = await this.findOne(id);
-    return this.todos.remove(todo);
+    return this.todosRepository.remove(todo);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,40 +9,33 @@ import { Repository } from 'typeorm';
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private users: Repository<User>,
+    private usersRepository: Repository<User>,
   ){}
 
   create(createUserDto: CreateUserDto) {
-    const newUser = this.users.create(createUserDto);
-    return this.users.save(newUser);
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
   }
 
   findAll() {
-    return this.users.find();
+    return this.usersRepository.find();
   }
 
   async findOne(id: number) {
-    const user = await this.users.find({
+    return this.usersRepository.find({
       where: {
         id: id
       }
-    });
-    if(Object.keys(user).length === 0) {
-      throw new HttpException("User with such id not found", HttpStatus.NOT_FOUND)
-    }; 
-    return user;
+    })
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
     const user = await this.findOne(id);
-    Object.keys(updateUserDto).forEach(key => {
-      user[0][key] = updateUserDto[key];
-    })
-    return this.users.save(user);
+    return this.usersRepository.save({...user, ...updateUserDto});
   }
 
   async remove(id: number) {
     const user = await this.findOne(id);
-    return this.users.remove(user);
+    return this.usersRepository.remove(user);
   }
 }
